5f80d78dcc699a1b18fcfb499f80ba4b2a780f5b
Gayloc <gu18647403665@outlook.com>
2023-09-26 21:08:30 +0800

    feat:优化显示图片的功能，添加返回顶部的按钮

--- simple-home-page/js/information.js
+++ simple-home-page/js/information.js
@@ -1,49 +1,67 @@
-function setbackground(content) {
-    if (content.status) {
-        let list = Object.values(content.data);
+var index=0;
+list=new Array;
 
+
+function setCardBox(content) {
+    if (content.status) {
+        list = Object.values(content.data);
         let card_box = document.createElement("div");
         let card_box_class = document.createAttribute("class");
         card_box_class.nodeValue = "card-box";
         card_box.attributes.setNamedItem(card_box_class);
-
-
-
         let body = document.querySelector("body");
         body.appendChild(card_box);
 
-        for (let i = 0; i < 10; i++) {
-            let card = document.createElement("div");
-            let card_class = document.createAttribute("class");
-            card_class.nodeValue = "card";
-            card.attributes.setNamedItem(card_class);
-            let img = document.createElement("img");
-            let img_src = document.createAttribute("src");
+        let more = document.createElement("div");
+        let more_class = document.createAttribute("class");
+        let more_action = document.createAttribute("onclick");
+        more_action.nodeValue="addContent()";
+        more_class.nodeValue="more";
+        more.attributes.setNamedItem(more_class);
+        more.attributes.setNamedItem(more_action);
+        more.innerText="加载更多"
+        card_box.appendChild(more);
 
-            let title = document.createElement("a");
-            let copyright = document.createElement("p");
-
-            let luckynums =Math.floor(Math.random() * list.length)
-            img_src.nodeValue = "https://www.bing.com" + list[luckynums].url;
-            copyright.innerHTML=list[luckynums].copyright;
-            title.innerHTML=list[luckynums].title;
-
-            let title_href = document.createAttribute("href");
-            title_href.nodeValue=list[luckynums].copyrightlink;
-            title.attributes.setNamedItem(title_href);
-
-            img.attributes.setNamedItem(img_src);
-            card.appendChild(img);
-            card.appendChild(title);
-            card.appendChild(copyright);
-            card_box.appendChild(card);
-
-        }
-
-
+        addContent();
+        
     }
+}
+
+function addCard(card_box, img_params, title_params, copyright_params, copyrightlink_params) {
+    let card = document.createElement("div");
+    let card_class = document.createAttribute("class");
+    card_class.nodeValue = "card";
+    card.attributes.setNamedItem(card_class);
+    let img = document.createElement("img");
+    let img_src = document.createAttribute("src");
+
+    let title = document.createElement("a");
+    let copyright = document.createElement("p");
+
+    img_src.nodeValue = "https://www.bing.com/"+img_params;
+    copyright.innerHTML = copyright_params;
+    title.innerHTML = title_params;
+
+    let title_href = document.createAttribute("href");
+    title_href.nodeValue = "https://www.bing.com/"+copyrightlink_params;
+    title.attributes.setNamedItem(title_href);
+
+    img.attributes.setNamedItem(img_src);
+    card.appendChild(img);
+    card.appendChild(title);
+    card.appendChild(copyright);
+    card_box.appendChild(card);
+}
+
+function addContent() {
+    let card_box=document.querySelector(".card-box");
+    for (let i=0 ; i < 5; i++) {
+        addCard(card_box,list[index].url,list[index].title,list[index].copyright,list[index].copyrightlink);
+        index++;
+    }
+
 }
 
 fetch("https://raw.onmicrosoft.cn/Bing-Wallpaper-Action/main/data/zh-CN_all.json")
     .then((response) => response.json())
-    .then((data) => setbackground(data));
+    .then((data) => setCardBox(data));
