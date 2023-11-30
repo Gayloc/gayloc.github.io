c38cb45a2813f1ecc6ed4fb5fb03451929b019d5
Gayloc <gu18647403665@outlook.com>
2023-11-30 18:07:31 +0800

    更新网站内容

--- friends/js/script.js
+++
@@ -1,9 +0,0 @@
-function like(data) {
-    if (data.classList == "good") {
-        data.src = "./src/good-fill.svg"
-        data.classList = "good-fill"
-    } else {
-        data.src = "./src/good.svg"
-        data.classList = "good"
-    }
-}
